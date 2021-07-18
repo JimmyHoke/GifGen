3ac16bb0ac9fb8a4cd787407f1fb57b896e9bf6e
Jimmy Hoke <jimmy.hoke@yahoo.com>
2021-07-18 15:36:11 -0500

    Initial commit

---
+++ GifGen/Data/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GifGen.Data
+{
+    public class Constants
+    {
+        public const string OutPutPath = "/var/www/html/gif/";
+        public const string VideoPath = "/var/gifgen/";
+        public const string BaseUrl = "https://jimmyhoke.net/gif/";
+    }
+}
